aae2d6546f32c11ee3492f7e45ca0e09b6b094c3
Jack Meng <jackmeng0814@gmail.com>
2022-08-29 11:54:43 -0400

    `add`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,19 @@ repositories {
     mavenCentral()
 }
 
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = "https://maven.pkg.github.com/exoad/cli-java"
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
     implementation("com.google.guava:guava:30.1.1-jre")
