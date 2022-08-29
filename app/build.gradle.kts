1966864a1f7ad272bf35e90962616b350495ac5e
Jack Meng <jackmeng0814@gmail.com>
2022-08-29 11:59:35 -0400

    fix uri to string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = "https://maven.pkg.github.com/exoad/cli-java"
+            url = uri("https://maven.pkg.github.com/exoad/cli-java")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
